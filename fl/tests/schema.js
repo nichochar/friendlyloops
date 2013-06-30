2e16560ec1170c2f052845a5a813da0c674ed860
Alex Ehrnschwender <ehrndog@gmail.com>
2013-06-29 17:01:30 -0700

    accounts-facebook login and user creation

---
+++ fl/tests/schema.js
@@ -0,0 +1,32 @@
+
+Meteor.user = {
+    _id:    "asdfasdfasdf",
+    services: {
+        facebook: {
+            id: "218293811",
+            name: "asdlkfjasdf",
+            username: "ehrns"
+        }
+    },
+    createdAt: "timestamp"
+};    
+
+
+Room = {
+
+    _id:    "aslkdfjlasjfw8efu",
+    name:   "My Room",
+    owner_id: "asldfkjasldfjaa",
+    createdAt: "timestamp"
+
+};
+
+
+
+Loop = {
+    _id: "asdlfkjasdfas",
+    name: "asdfljasldfas",
+    length: 12,
+    url: "/asdfk/asdfk/asd.mp3",
+    createdAt: "timestamp"
+};
